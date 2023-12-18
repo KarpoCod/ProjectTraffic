aaf55b6729243ceb4b65f2b67ebcbc1ae0f7eef0
KarpoCod <2oivit160807@gmail.com>
2023-12-18 19:14:55 +0300

    v

--- Assets/Scripts/UpdateManager.cs
+++ Assets/Scripts/UpdateManager.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class UpdateManager : MonoBehaviour
 {
+    public light light_pref;
     public light[] lights;
     //public road[] roads;
     public float t = 1;
@@ -12,9 +13,14 @@ public class UpdateManager : MonoBehaviour
     public bool pause = false;
     public GameObject pause_but;
     public GameObject[] edit_tools;
+    public Camera Cam;
 
     void Update()
     {
+        if (!work && Input.GetMouseButtonDown(0))
+        {
+            Clicked();
+        }
         if (work && !pause)
         {
             if (timer < 0)
@@ -41,5 +47,30 @@ public class UpdateManager : MonoBehaviour
     public void set_pause(bool w)
     {
         pause = w;
+    }
+
+    private void Clicked()
+    {
+        light[] li = new light[lights.Length + 1];
+        Debug.Log(Input.mousePosition);
+        Vector3 light_pos = new Vector3();
+        Vector3 mPos = Input.mousePosition; 
+        Debug.Log(Cam.ScreenToWorldPoint(mPos));
+        Ray ray = Cam.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            light_pos = hit.point;
+        }
+        if (lights.Length == 0) { lights = new light[1]; lights[0] = Instantiate(light_pref, light_pos, Quaternion.identity); }
+        else
+        {
+            for (int i = 0; i < lights.Length; i++)
+            {
+                li[i] = lights[i];
+            }
+            lights[lights.Length - 1] = Instantiate(light_pref, light_pos, Quaternion.identity);
+        }
     }
 }
